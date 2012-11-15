f8ae1ac205828db48ad2e894f152c198e67db28f
Teresa J McGuffey Hardy <tjhardy@isu.edu>
2012-11-15 07:26:59 -0700

    Update phpcodedemos/225demos/week4/03-add-get-products.php

--- phpcodedemos/225demos/week4/03-add-get-products.php
+++ phpcodedemos/225demos/week4/03-add-get-products.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 
 // create our connection object by filling in our connection string params
-$conn = new mysqli('localhost','tjhardy','t1g3r1','tjhardy');
+$conn = new mysqli('');
 
 // 'connect_error()' returns the last connection type error, if there is one
 if (mysqli_connect_errno() !=0)

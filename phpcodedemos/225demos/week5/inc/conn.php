25e5bdc68235cf83ce677bb88efe3c74eaf70bde
Teresa J McGuffey Hardy <tjhardy@isu.edu>
2012-11-15 07:29:35 -0700

    Update phpcodedemos/225demos/week5/inc/conn.php

--- phpcodedemos/225demos/week5/inc/conn.php
+++ phpcodedemos/225demos/week5/inc/conn.php
@@ -1,6 +1,6 @@
 <?php 
 // create our connection object by filling in our connection string params
-$conn = new mysqli('localhost','tjhardy','t1g3r1','tjhardy');
+$conn = new mysqli('');
 
 // 'connect_error()' returns the last connection type error, if there is one
 if (mysqli_connect_errno() !=0)
